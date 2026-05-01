7ba71110a4eac10ec17471d77d0bc323b65563ac
Mazha0309 <mazha20110309@outlook.com>
2026-05-01 16:49:40 +0800

    fix(webui): dictionary service accepts params object instead of positional args

--- web/src/services/dictionary.js
+++ web/src/services/dictionary.js
@@ -1,7 +1,7 @@
 import api from '../utils/api';
 
-export async function getDictionaries(type, search) {
-  return api.get('/dictionaries', { params: { type, search } });
+export async function getDictionaries(params = {}) {
+  return api.get('/dictionaries', { params });
 }
 
 export async function createDictionary(type, data) {
